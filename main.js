2b41fd5603af9c5a63d0cd28500cd5af1e854690
Natalie HV <114947915+Natalie-hvb@users.noreply.github.com>
2023-07-31 15:26:32 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const btn = document.getElementById('mybtn')
-const myform = document.getElementById('my-form')
+const btn = document.getElementById('my-btn')
+const myForm = document.getElementById('my-form')
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('click');
-    myform.style.display = 'none';
-});
-
+var err = document.getElementById('err');
+var result = document.getElementById('result');
+var btnReset = document.getElementById('btnReset');
 let firstName = document.getElementById('icon_prefix');
-let phoneNum = document.getElementById('phoneNum');
-let story = document.getElementById('story')
-console.log(firstName)
+let phoneNum = document.getElementById('icon_telephone');
+let story = document.getElementById('icon_prefix2')
+
+function oneCheckedBox(checkbox_id) {
+    if (document.getElementById(checkbox_id).checked) {
+        for (var i = 1; i <= 2; i++) {
+            document.getElementById("CheckId"+i).checked = false;
+        }
+        document.getElementById(checkbox_id).checked = true;
+    }
+}
 
 var person = {
     firstName: [],
@@ -18,9 +23,32 @@ var person = {
     story: []
 };
 
-function success() {
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    myForm.style.display = "none"
+    var valueName = firstName.value;
+    var valueNum = phoneNum.value;
+    var valueStory = story.value;
+   
+
+    if(valueName !== '' && valueNum !== '' && valueStory !== ''){
+        err.innerText = ''
+        title.innerText = "Your application successfully sent!"
+        person.firstName.push(valueName),
+        person.phoneNum.push(valueNum),
+        person.story.push(valueStory),        
+        success()
+       
+    } else {
+        title.innerText = ''
+        err.innerText = 'The form is not full'
+    }
+});
+
+function success(){
     result.innerHTML = '';
-    for (let i = 0; i < person.firstName.length && i < person.phoneNum.length && i < person.story.length; index++) {
-        result.innerHTML += '<ul>' + '<li>' + person.firstName[i] + '/li' + '<li>' + person.phoneNum[i] + '</li>' + '<li>' + person.story[i] + '</li>' + '</ul'
+    for(var i=0; i<person.firstName.length && i<person.phoneNum.length && i<person.story.length; i++){
+        result.innerHTML += '<ol>' + '<li>' + person.firstName[i] + '</li>' + '<li>' + person.phoneNum[i] + '</li>' +  '<li>' + person.story[i] + '</li>' + '</ol>'
     }
 }
+
